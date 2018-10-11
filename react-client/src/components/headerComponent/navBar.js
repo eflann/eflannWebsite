882db2d6186e5310c90a06fa1c2c088f5480fcf3
Erin Flannery <eflannery@twilio.com>
2018-10-11 20:54:47 +0300

    headshot + navbar

--- react-client/src/components/headerComponent/navBar.js
+++ react-client/src/components/headerComponent/navBar.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
 
 class NavBar extends Component {
   render() {
     return (
-      <header>
-        <ul id="headerButtons">
-          <li className="navButton"><Link to="">Home</Link></li>
-        </ul>
-      </header>
-    )
+      <Navbar inverse collapseOnSelect navbar-static-top className='nav-bar'>
+		  <Navbar.Collapse>
+		    <Nav pullRight>
+		      <NavItem eventKey={1} href="#">
+		        About
+		      </NavItem>
+		      <NavItem eventKey={2} href="#">
+		        US Travel
+		      </NavItem>
+		      <NavItem eventKey={2} href="#">
+		        International Travel
+		      </NavItem>
+		    </Nav>
+		  </Navbar.Collapse>
+	  </Navbar>
+    );
   }
 }
 
--- react-client/src/components/pages/homePage.js
+++ react-client/src/components/pages/homePage.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
+import { Jumbotron, Image } from 'react-bootstrap';
+import eflannery from './erin-headshot.jpg'
 
 class HomePage extends Component {
   render() {
     return (
       <div>
-        Home page
+        <Jumbotron>
+        	<Image className='headshot' src={eflannery} circle/>
+		  <h1>Erin Flannery</h1>
+		  <p>
+		    I'm currently a software engineer at Twilio. I graduated in 2017 from the University of Michigan with a BSE in Computer Science Engineering.
+		  </p>
+		</Jumbotron>
       </div>
     )
   }
